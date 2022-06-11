7934e590f8f0ca044dfdd11a1b107c485c4d5f89
Said Yeter <kordiseps@gmail.com>
2022-06-11 15:52:42 +0300

    Make mssql db adapter

--- src/DataAccess/LibraryDbContext.ts
+++ src/DataAccess/LibraryDbContext.ts
@@ -1,19 +1,24 @@
-import { MsSqlAdapter } from "../Base/Adapter";
+import { DbAdapterConstructorArgument, MsSqlAdapter } from "../Base/Adapter";
 import { BaseDbSet, TableSaveChanges } from "../Base/Core";
 import { Author, AuthorTable, AuthorTableNumbers, AuthorTableStrings } from "./Models/Author";
 import { Book, BookTable, BookTableNumbers, BookTableStrings } from "./Models/Book";
 import { Publisher, PublisherTable, PublisherTableNumbers, PublisherTableStrings } from "./Models/Publisher";
 
-const adapter= new MsSqlAdapter('')
 
-const book = BaseDbSet<Book, BookTableStrings, BookTableNumbers>('book',adapter,BookTable);
-const author = BaseDbSet<Author, AuthorTableStrings, AuthorTableNumbers>('author',adapter,AuthorTable);
-const publisher = BaseDbSet<Publisher, PublisherTableStrings, PublisherTableNumbers>('publisher',adapter,PublisherTable);
+function init(connectionInfo: DbAdapterConstructorArgument) {
+    const adapter = new MsSqlAdapter(connectionInfo)
 
-function SaveChanges(){
-    TableSaveChanges(book.Changes, 'book', BookTable, adapter)
-    TableSaveChanges(author.Changes, 'author', AuthorTable, adapter)
-    TableSaveChanges(publisher.Changes, 'publisher', PublisherTable, adapter)
+    const book = BaseDbSet<Book, BookTableStrings, BookTableNumbers>('book', adapter, BookTable);
+    const author = BaseDbSet<Author, AuthorTableStrings, AuthorTableNumbers>('author', adapter, AuthorTable);
+    const publisher = BaseDbSet<Publisher, PublisherTableStrings, PublisherTableNumbers>('publisher', adapter, PublisherTable);
+
+    async function SaveChanges() : Promise<void>{
+        await TableSaveChanges(book.Changes, 'book', BookTable, adapter)
+        await TableSaveChanges(author.Changes, 'author', AuthorTable, adapter)
+        await TableSaveChanges(publisher.Changes, 'publisher', PublisherTable, adapter)
+    }
+
+    return { book, author, publisher, SaveChanges }
 }
 
-export default { book, author, publisher, SaveChanges }
+export default { init }
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,39 @@
-import db from "./DataAccess/LibraryDbContext";
+import dbAdapter from "./DataAccess/LibraryDbContext";
 
-db.book.Add({
-    InsertedAt: new Date(),
-    Name: 'Lord of the Rings',
-    AuthorId: 23,
-    PublisherId: 2,
-})
+(async function () {
 
-db.SaveChanges()
+    // const a = {
+    //     database: 'ef4js',
+    //     server: 'localhost,1433',
+    //     user: 'sa',
+    //     password: 'MyPass@word'
+    // }
 
-const some = db.book
+
+    const conenctionString = 'Server=localhost,1433;Database=ef4js;User Id=sa;Password=MyPass@word;TrustServerCertificate=True;'
+
+    const db = dbAdapter.init(conenctionString)
+
+    const all = await db.book
     .Where()
-    .BiggerThen('PublisherId', 25)
-    .Contains('Name', 'ali')
     .GetAll()
 
-console.log(some);
+    console.log(all);
+
+
+    const first = await db.book
+        .Where()
+        .BiggerThen('PublisherId', 4)
+        .GetFirst()
+
+    console.log(first);
+
+    db.book.Add({
+        InsertedAt: new Date(),
+        Name: 'Dice man',
+        AuthorId: 12,
+        PublisherId: 2,
+    })
+
+    await db.SaveChanges()
+})()
